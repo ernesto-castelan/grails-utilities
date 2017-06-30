ad7c2137028abb4cd62c3aea1be92a2e3110334b
Ernesto Castelan <ernesto.castelan@vincoorbis.com>
2017-06-30 18:46:11 -0500

    First version of Uncertain class

---
+++ src/groovy/com/vincoorbis/grails/Uncertain.groovy
@@ -0,0 +1,70 @@
+package com.vincoorbis.grails
+
+class Uncertain<T> {
+
+    private T instance
+    private Map errors
+
+    /* Groovy does not respect the private keyword. This is a way to disable the named argument constructor
+     * and somewhat prevent accidental instantiation with the default constructor. Don't judge me.
+     */
+    private Uncertain(Boolean b) {}
+
+    static <T> Uncertain<T> success(T instance) {
+        if(instance == null) throw new IllegalArgumentException("Instance can't be null")
+
+        Uncertain<T> result = new Uncertain<T>(true)
+        result.instance = instance
+        result.errors = null
+
+        result
+    }
+
+    static <T> Uncertain<T> failure(Map errors = [:]) {
+        if (errors == null) throw new IllegalArgumentException("Errors can't be null")
+
+        Uncertain<T> result = new Uncertain<T>(true)
+        result.instance = null
+        result.errors = errors
+
+        result
+    }
+
+    static <T> Uncertain<T> failure(Exception exception) {
+        if (exception == null) throw new IllegalArgumentException("Exception can't be null")
+
+        Uncertain<T> result = new Uncertain<T>(true)
+        result.instance = null
+        result.errors = [(exception.class.simpleName):exception.message]
+
+        result
+    }
+
+    // Tests instance strictly against null
+    static <T> Uncertain<T> testNull(T instance, Map errors) {
+        if(instance == null) failure(errors)
+        else success(instance)
+    }
+
+    // Tests instance using The Groovy Truth (TM)
+    static <T> Uncertain<T> testTruth(T instance, Map errors) {
+        if(!instance) failure(errors)
+        else success(instance)
+    }
+
+    T getInstance() {
+        instance
+    }
+
+    Map getErrors() {
+        errors
+    }
+
+    boolean isNull() {
+        instance == null
+    }
+
+    boolean asBoolean() {
+        instance
+    }
+}
---
+++ test/unit/com/vincoorbis/grails/UncertainSpec.groovy
@@ -0,0 +1,121 @@
+package com.vincoorbis.grails
+
+import grails.test.mixin.TestMixin
+import grails.test.mixin.support.GrailsUnitTestMixin
+import spock.lang.Specification
+
+@TestMixin(GrailsUnitTestMixin)
+class UncertainSpec extends Specification {
+
+    void "'success' method creates a succesfull instance"() {
+        when:"Using the success method"
+            Uncertain<String> result = Uncertain.success("hello")
+        then:"The instance is correct"
+            result.instance == "hello"
+            result.errors == null
+    }
+
+    void "'success' method with falsy param creates a succesfull instance"() {
+        when:"Using the success method"
+            Uncertain<String> result = Uncertain.success("")
+        then:"The instance is correct"
+            result.instance == ""
+            result.errors == null
+    }
+
+    void "'success' method with null parameter raises an exception"() {
+        when:"Using the success method"
+            Uncertain<String> result = Uncertain.success(null)
+        then:"An exception is raised"
+            thrown IllegalArgumentException
+    }
+
+    void "'failure(map)' method creates an unsuccesfull instance"() {
+        when:"Using the failure method"
+            Uncertain<String> result = Uncertain.failure([property:"error.code"])
+        then:"The instance is correct"
+            result.instance == null
+            result.errors == [property:"error.code"]
+    }
+
+    void "'failure(exception)' method creates an unsuccesfull instance"() {
+        given:"An exception"
+            Exception exceptionInstance = new IllegalStateException("message")
+        when:"Using the failure method"
+            Uncertain<String> result = Uncertain.failure(exceptionInstance)
+        then:"The instance is correct"
+            result.instance == null
+            result.errors == [IllegalStateException:"message"]
+    }
+
+    void "'failure' method with null parameter raises an exception"() {
+        when:"Using the failure method"
+            Uncertain<String> result = Uncertain.failure(null)
+        then:"An exception is raised"
+            thrown IllegalArgumentException
+    }
+
+    void "'testNull' method evaluates correctly"() {
+        when:"Using the testNull method"
+            Uncertain<String> result = Uncertain.testNull(value, [property:"error.code"])
+        then:"The instance is correct"
+            result.instance == expectedInstance
+            result.errors == expectedErrors
+        where:
+            value       ||expectedInstance  |expectedErrors
+            "hello"     ||"hello"            |null
+            ""          ||""                |null
+            null        ||null              |[property:"error.code"]
+    }
+
+    void "'testNull' method with null parameters raises an exception"() {
+        when:"Using the testNull method"
+            Uncertain<String> result = Uncertain.testNull(null, null)
+        then:"An exception is raised"
+            thrown IllegalArgumentException
+    }
+
+    void "'testTruth' method evaluates correctly"() {
+        when:"Using the testTruth method"
+            Uncertain<String> result = Uncertain.testTruth(value, [property:"error.code"])
+        then:"The instance is correct"
+            result.instance == expectedInstance
+            result.errors == expectedErrors
+        where:
+            value       ||expectedInstance  |expectedErrors
+            "hello"     ||"hello"            |null
+            ""          ||null              |[property:"error.code"]
+            null        ||null              |[property:"error.code"]
+    }
+
+    void "'testTruth' method with null parameters raises an exception"() {
+        when:"Using the testTruth method"
+            Uncertain<String> result = Uncertain.testTruth(null, null)
+        then:"An exception is raised"
+            thrown IllegalArgumentException
+    }
+
+    void "'isNull' method evaluates correctly"() {
+        given:"An Uncertain instance"
+            Uncertain<String> result = Uncertain.testNull(value, [property:"error.code"])
+        expect:"The correct value"
+            result.isNull() == expectedResult
+        where:
+            value       ||expectedResult
+            "hello"     ||false
+            ""          ||false
+            null        ||true
+    }
+
+    void "'asBoolean' method evaluates correctly"() {
+        given:"An Uncertain instance"
+            Uncertain<String> result = Uncertain.testNull(value, [property:"error.code"])
+        expect:"The correct value"
+            !result == expectedResult
+        where:
+            value       ||expectedResult
+            "hello"     ||false
+            ""          ||true
+            null        ||true
+    }
+}
